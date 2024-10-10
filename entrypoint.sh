9ee889129362068571aa78ba25d9810875b498a4
Nathan <ndowens@artixlinux.org>
2024-10-10 01:09:55 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,2 @@
-#!/bin/sh -l
-
-sh -c "echo $*"
-
-sh -c "echo NAME: $INPUT_NAME"
-sh -c "echo FILE: $INPUT_FILE"
-
-sh -c "cat /README.md"
-sh -c "cat /github/workspace/README.md"
-#sh -c "cat /github/workspace/target/surefire-reports/TEST-com.xpand.java.CalcTest.xml"
-sh -c "cat /github/workspace/$INPUT_FILE"
-sh -c "find /github/workspace/ -type f"
-
-
-curl -H "Content-Type: multipart/form-data" -u $INPUT_JIRA_USERNAME:$INPUT_JIRA_PASSWORD -F "file=@/github/workspace/$INPUT_FILE" "$INPUT_JIRA_URL/rest/raven/1.0/import/execution/junit?projectKey=$INPUT_PROJECT_KEY"
+#!/bin/bash
+echo ""
